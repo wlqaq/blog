dc511961d80cbc3f5dd93835754aa917089935fb
wlqaq <1032902096@qq.com>
2021-08-23 13:32:05 +0800

    乱搞

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,21 @@
+
+@include('home.public.header')
+
+<script src="https://cdn.jsdelivr.net/npm/wangeditor@latest/dist/wangEditor.min.js"></script>
+
+<script src="{{asset('/home/js/vue.min.js')}}"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/axios/0.21.1/axios.min.js"></script>
+<article>
+
+    <div class="whitebg" style="background-color:#fff;">
+        大家好我是niee,90后的PHP工程师。
+
+    </div>
+
+
+    </div>
+
+</article>
+
+
+@include('home.public.footer')
